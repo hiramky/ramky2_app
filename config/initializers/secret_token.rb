ce656192ab4f85fd3cc9df510360b979bc42e1cc
hiramky <hiramky@yahoo.com>
2012-06-25 09:42:32 -0700

    third commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'f752bfa8aa7589fc6c1f49d16efebe5663cbdec85d664c2fce6aae955527f2b720c356388959d69bf62d35ec34ab93f7152261e29f48a5aabff5db6c3301535c'
---
+++ db/migrate/20120619062759_add_index_to_posts.rb
@@ -0,0 +1,5 @@
+class AddIndexToPosts < ActiveRecord::Migration
+  def change
+    add_index :microposts, [:user_id, :created_at]
+  end
+end
